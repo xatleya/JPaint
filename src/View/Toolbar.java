5243bdb77c4ac1237efc68074237616df31c713b
ISEN <ISEN@ISENMIC-N20T23U>
2016-03-16 10:06:55 +0100

    My Commit

--- src/View/Toolbar.java
+++ src/View/Toolbar.java
@@ -1,11 +1,26 @@
 package View;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JButton;
+import javax.swing.JSeparator;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 public class Toolbar extends JToolBar{
     public Toolbar() {
+        super(null, JToolBar.VERTICAL);
         JButton selectButton = new JButton("select");
         this.add(selectButton);
+        
+        //this.addSeparator(new Dimension(0,50));
+        JSeparator separator = new Toolbar.Separator();
+        separator.setOrientation(SwingConstants.HORIZONTAL);
+        //this.add(separator);
+        
+        JButton backgroundColor = new JButton();
+        backgroundColor.setEnabled(false);
+        backgroundColor.setBackground(Color.red);
+        this.add(backgroundColor);
     } 
 }
